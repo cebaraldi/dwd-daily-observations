c2f741a31f89742cbefdf15c3c17adb757d9c489
cebaraldi@gmail.com <cebaraldi@gmail.com>
2024-08-18 16:59:57 +0000

    Edited ceb_Layout

--- client_code/ceb_Layout/__init__.py
+++ client_code/ceb_Layout/__init__.py
@@ -21,7 +21,6 @@ class ceb_Layout(ceb_LayoutTemplate):
     """This method is called when the link is clicked"""
     open_form('Google_Maps')
 
-  def about_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     open_form('About')
 
