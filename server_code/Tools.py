d91a52ec9cc1e8f7721afb73ba60591afe4307eb
cebaraldi@gmail.com <cebaraldi@gmail.com>
2024-08-18 07:16:28 +0000

    Edited Tools

--- server_code/Tools.py
+++ server_code/Tools.py
@@ -7,18 +7,6 @@ import anvil.server
 #from datetime import datetime
 import pandas as pd
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-@anvil.server.callable
-def say_hello(name):
-  print("Hello, " + name + "!")
-  return 42
-
 @anvil.server.callable
 def check_file_existence(file_name):
   try:
